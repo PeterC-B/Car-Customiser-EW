835bc4e38b371810d1620f6a0892e0d52e2fbf0a
Peter Compton-Burnett <compton-burnett.p@etoncollege.org.uk>
2022-01-18 23:01:42 +0000

    18.01

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -16,24 +16,54 @@ struct ContentView: View {
     @State private var turboPackage = false
     @State private var engineUpgradePackage = false
     
-    @State private var money = 200
+    @State private var money = 1000
+    @State private var remainingTime = 30
+    
+    @State var timeRemaining = 10
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var timeUp = false
+    
+    var exhaustPackageEnabled: Bool {
+        return exhaustPackage ? true : money >= 500 ? true : false
+    }
+    
+    var tiresPackageEnabled: Bool {
+        return tiresPackage ? true : money >= 500 ? true : false
+    }
+    
+    var turboPackageEnabled: Bool {
+        return turboPackage ? true : money >= 500 ? true : false
+    }
+    
+    var engineUpgradePackageEnabled: Bool {
+        return engineUpgradePackage ? true : money >= 500 ? true : false
+    }
     
     
     var body: some View {
         let exhaustPackageBinding = Binding<Bool> (
             get : { self.exhaustPackage},
             set: { newValue in
-                if money < 45{
+                if remainingTime <= 0{
                     return
                 }
-                else {
-                    money -= 45
-                }
-                self.exhaustPackage = newValue
-                if newValue == true {
+                if newValue == true{
+                    if money < 45{
+                        return
+                    }
+                    else {
+                        money -= 45
+                    }
+                    self.exhaustPackage = newValue
                     starterCars.cars[selectedCar].topSpeed += 5
-                } else {
+                    
+                        
+                    }
+                else {
+                    money += 45
+                    self.exhaustPackage = newValue
                     starterCars.cars[selectedCar].topSpeed -= 5
+                        
                 }
             }
         )
@@ -41,17 +71,26 @@ struct ContentView: View {
         let tiresPackageBinding = Binding<Bool> (
             get : { self.tiresPackage},
             set: { newValue in
-                if money < 19{
+                if remainingTime <= 0{
                     return
                 }
-                else {
-                    money -= 19
-                }
-                self.tiresPackage = newValue
-                if newValue == true {
+                if newValue == true{
+                    if money < 19{
+                        return
+                    }
+                    else {
+                        money -= 19
+                    }
+                    self.tiresPackage = newValue
                     starterCars.cars[selectedCar].handling += 2
-                } else {
+                    
+                        
+                    }
+                else {
+                    money += 19
+                    self.tiresPackage = newValue
                     starterCars.cars[selectedCar].handling -= 2
+                        
                 }
             }
         )
@@ -59,17 +98,26 @@ struct ContentView: View {
         let turboPackageBinding = Binding<Bool> (
             get : { self.turboPackage},
             set: { newValue in
-                if money < 35{
+                if remainingTime <= 0{
                     return
                 }
-                else {
-                    money -= 35
-                }
-                self.turboPackage = newValue
-                if newValue == true {
+                if newValue == true{
+                    if money < 35{
+                        return
+                    }
+                    else {
+                        money -= 35
+                    }
+                    self.turboPackage = newValue
                     starterCars.cars[selectedCar].topSpeed += 21
-                } else {
+                    
+                        
+                    }
+                else {
+                    money += 35
+                    self.turboPackage = newValue
                     starterCars.cars[selectedCar].topSpeed -= 21
+                        
                 }
             }
         )
@@ -77,54 +125,100 @@ struct ContentView: View {
         let engineUpgradePackageBinding = Binding<Bool> (
             get : { self.engineUpgradePackage},
             set: { newValue in
-                if money < 60{
+                if remainingTime <= 0{
                     return
                 }
-                else {
-                    money -= 60
-                }
-                self.engineUpgradePackage = newValue
-                if newValue == true {
+                if newValue == true{
+                    if money < 60{
+                        return
+                    }
+                    else {
+                        money -= 60
+                    }
+                    self.engineUpgradePackage = newValue
                     starterCars.cars[selectedCar].topSpeed += 15
                     starterCars.cars[selectedCar].acceleration -= 0.75
-                } else {
+                    
+                        
+                    }
+                else {
+                    money += 60
+                    self.engineUpgradePackage = newValue
                     starterCars.cars[selectedCar].topSpeed -= 15
                     starterCars.cars[selectedCar].acceleration += 0.75
+                        
                 }
             }
         )
-        
-        Form {
-            VStack(alignment: .leading, spacing: 20){
-                Text("\(starterCars.cars[selectedCar].getDetails())")
-                    .padding()
-                Text("Money \(money)")
-                    .padding()
-                Button("Next Car", action: {
-                    if selectedCar+1 < self.starterCars.cars.count {
-                        selectedCar += 1
+        VStack {
+            Text("\(remainingTime)")
+                .onReceive(timer) { _ in
+                    if self.remainingTime > 0 {
+                        self.remainingTime -= 1
                     }
-                    else {
-                        selectedCar = 0
-                    }
-                    self.exhaustPackage = false
-                    self.tiresPackage = false
-                    self.turboPackage = false
-                    self.engineUpgradePackage = false
-                    //selectedCar = Int.random(in:0..<self.starterCars.cars.count)
-                })
-            }
-            Section {
-                Toggle("Exhaust Package", isOn: exhaustPackageBinding)
-                Toggle("Tires Package", isOn: tiresPackageBinding)
-                Toggle("Turbo Package", isOn: turboPackageBinding)
-                Toggle("Engine Upgrade Package", isOn: engineUpgradePackageBinding)
+                    
+                }
+                .foregroundColor(.red)
+            Form {
+                VStack(alignment: .leading, spacing: 20){
+                    Text("\(starterCars.cars[selectedCar].getDetails())")
+                        .padding()
+                    
+                    Button("Next Car", action: {
+                        if remainingTime <= 0{
+                            return
+                        }
+                        if selectedCar+1 < self.starterCars.cars.count {
+                            selectedCar += 1
+                        }
+                        else {
+                            selectedCar = 0
+                        }
+                        resetValues()
+                        self.exhaustPackage = false
+                        self.tiresPackage = false
+                        self.turboPackage = false
+                        self.engineUpgradePackage = false
+                        //selectedCar = Int.random(in:0..<self.starterCars.cars.count)
+                    })
+                }
+                Section {
+                    Toggle("Exhaust Package", isOn: exhaustPackageBinding)
+                    Toggle("Tires Package", isOn: tiresPackageBinding)
+                    Toggle("Turbo Package", isOn: turboPackageBinding)
+                    Toggle("Engine Upgrade Package", isOn: engineUpgradePackageBinding)
+                }
             }
         }
         
         
+        Text("\(timeRemaining)")
+                    .onReceive(timer) { _ in
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1
+                        }
+                        else {
+                            timeUp = true
+                        }
+                    }
+        
+        Text("Remaining Funds: \(money)")
+            .foregroundColor(.red)
+            .baselineOffset(20)
+        
+        
 
     }
+    
+    func resetValues() {
+        money = 1000
+        exhaustPackage = false
+        tiresPackage = false
+        turboPackage = false
+        engineUpgradePackage = false
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- CarCustomiserUITests/CarCustomiserUITests.swift
+++ CarCustomiserUITests/CarCustomiserUITests.swift
@@ -1,4 +1,4 @@
-//
+2//
 //  CarCustomiserUITests.swift
 //  CarCustomiserUITests
 //
@@ -10,7 +10,7 @@ import XCTest
 class CarCustomiserUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Put setup code here. This  method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
@@ -22,13 +22,38 @@ class CarCustomiserUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testWhenBoughtTiresAndExhaustPackageOtherTwoUpgradesAreDisabled() throws {
         let app = XCUIApplication()
         app.launch()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //act
+        
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.switches["Exhaust Package"]/*[[".cells[\"Exhaust Package\"].switches[\"Exhaust Package\"]",".switches[\"Exhaust Package\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.switches["Tires Package"]/*[[".cells[\"Tires Package\"].switches[\"Tires Package\"]",".switches[\"Tires Package\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        
+        //assert
+        XCTAssertEqual(tablesQuery/*@START_MENU_TOKEN@*/.switches["Turbo Package"]/*[[".cells[\"Turbo Package\"].switches[\"Turbo Package\"]",".switches[\"Turbo Package\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.isEnabled, false)
+        XCTAssertEqual(tablesQuery/*@START_MENU_TOKEN@*/.switches["Engine Upgrade Package"]/*[[".cells[\"Engine Upgrade Package\"].switches[\"Engine Upgrade Package\"]",".switches[\"Engine Upgrade Package\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.isEnabled, false)
+        
+    }
+    
+    func testWhenClickedNextCarAnotherCarSelected() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let current = car.model
+        //act
+        XCUIApplication().tables/*@START_MENU_TOKEN@*/.buttons["Next Car"]/*[[".cells[\"    Make: Ariel\\n    Model: Atom\\n    Top Speed: 149mph\\n    Acceleration (0-60 in): 2.1s\\n    Handling: 9, Next Car\"].buttons[\"Next Car\"]",".buttons[\"Next Car\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        //assert
+        XCTAssertNotEqual(car.model, current)
     }
 
     func testLaunchPerformance() throws {
